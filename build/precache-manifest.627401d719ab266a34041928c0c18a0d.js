32b73c059baadc5501dd9858217eed6e19cbc4ab
Sam Creydt <sam.creydt@linium.com>
2020-03-06 21:43:50 -0500

    adding files for github pages

---
+++ build/precache-manifest.627401d719ab266a34041928c0c18a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914257a5dc371e5d7eed0b8e8483f1e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1eb2b3493dfa44338d06",
+    "url": "/monsters-rolodex/static/css/main.6f5e1e06.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb2b3493dfa44338d06",
+    "url": "/monsters-rolodex/static/js/main.84e04eb5.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.componet';
+
+import './App.css';
+
+class App extends Component{
+  // Constructor runs first before anything is called.
+  // This is a life cycle method, like "OnInit"
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  // This is a life cycle method, it's like "OnLoad"
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}));
+  }
+
+  // This is a function of the class 'App'
+  // We use functions, like this because we can use it other places. We can now call this function in different areas
+  // Write code onces and reuse it somewhere else
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
+  // This is a life cycle method, like the ability to "Paint" the screen
+  render(){
+
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1> Monsters Rolodex </h1>
+        
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
+
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+
+export default App;
